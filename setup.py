d2b851436d1b76f31fe32dd37a427678fe568af1
Kurt Rose <doublereedkurt@gmail.com>
2013-08-14 04:08:54 -0700

    updating setup.py for cheeseshop registration

--- setup.py
+++ setup.py
@@ -5,6 +5,15 @@ _faststat = Extension('_faststat', sources=['faststat/_faststat.c'])
 setup(
     name='faststat',
     version='0.1',
+    author="Kurt Rose",
+    author_email="kurt@kurtrose.com",
     description='fast online statistics collection',
+    license="MIT",
+    url="http://github.com/doublereedkurt/faststat",
+    long_description='...',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+    ],
     packages=['faststat'],
     ext_modules=[_faststat])
