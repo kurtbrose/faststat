fb32d4b9fe5a33cbb7be301c1afcfd11cebd4c42
Kurt Rose <doublereedkurt@gmail.com>
2013-08-14 00:39:27 -0700

    got setup.py install working properly with the extension module

---
+++ faststat/__init__.py
@@ -0,0 +1,1 @@
+from faststat import *
--- setup.py
+++ setup.py
@@ -6,4 +6,5 @@ setup(
     name='faststat',
     version='0.1',
     description='fast online statistics collection',
+    packages=['faststat'],
     ext_modules=[_faststat])
