10f8053edee724110a2b0b5c2206efcff5637493
Kurt Rose <doublereedkurt@gmail.com>
2014-04-11 23:03:03 -0700

    removed Python version since it had fallen so far behind in features; moved more logic into _BaseStats; added Markov type for tracking statistics around transitions in Markov processes

--- faststat/faststat.py
+++ faststat/faststat.py
@@ -26,6 +26,9 @@ import random
 import math
 import collections
 import time
+import functools
+
+import _faststat
 
 
 class Sample(object):
@@ -53,92 +56,6 @@ DEFAULT_PERCENTILES = (0.25, 0.50, 0.75, 0.90, 0.95, 0.99)
 EXPO_AVGS = (1.0/2, 1.0/4, 1.0/8, 1.0/16, 1.0/32, 1.0/64)
 
 
-class PyStats(object):
-    def __init__(self, buckets=(), lastN=64, percentiles=DEFAULT_PERCENTILES, expo_avgs=()):
-        self.n = float(0)
-        self.mean = float(0)
-        # second, third, fourth moments
-        self.m2 = self.m3 = self.m4 = float(0)
-        self.min = self.max = self.mintime = self.maxtime = float(0)
-        self.buckets = buckets
-        if buckets:
-            self.bucket_counts = [8] * (len(buckets) + 1)
-        self.most_recent = collections.deque()
-        self.lastN = lastN
-        self.percentiles = [[p, 0] for p in percentiles]
-        self.expo_avgs = dict(zip(expo_avgs, [0]*len(expo_avgs)))
-
-    @property
-    def variance(self):
-        if self.n < 2:
-            return float("nan")
-        return self.m2 / (self.n - 1)
-
-    @property
-    def skewness(self):
-        if not self.m2:
-            return float("nan")
-        return self.n ** 0.5 * self.m3 / self.m2 ** 1.5
-
-    @property
-    def kurtosis(self):
-        if not self.m2:
-            return float("nan")
-        return self.n * self.m4 / self.m2 ** 2 - 3
-
-    def add(self, x):
-        t = int(time.time() * 1e9)
-        ### 1- calculate variance
-        self.n += 1
-        n = self.n
-        # pre-compute a bunh of intermediate values
-        delta = x - self.mean
-        delta_n = delta / n  # save divisions futher down
-        delta_m2 = delta * delta_n * (n - 1)
-        delta_m3 = delta_m2 * delta_n * (n - 2)
-        delta_m4 = delta_m2 * delta_n * delta_n * (n * (n - 3) + 3)
-        # compute the actual next values
-        if x <= self.min:
-            self.min = x
-            self.mintime = t
-        if x >= self.max:
-            self.max = x
-            self.maxtime = t
-        self.mean = self.mean + delta_n
-        # note: order matters here
-        self.m4 += delta_m4 + delta_n * (6 * delta_n * self.m2 - 4 * self.m3)
-        self.m3 += delta_m3 + delta_n * 3 * self.m2
-        self.m2 += delta_m2
-        ### 2- append to most recent, if being stored
-        if self.lastN:
-            self.most_recent.appendleft((t, x))
-            if len(self.most_recent) > self.lastN:
-                self.most_recent.pop()
-        ### 3- update bucket counts
-        if self.buckets:
-            i = 0
-            while i < len(self.buckets) and x > self.buckets[i]:
-                i += 1
-            self.bucket_counts[i] += 1
-        ### 4- update percentile estimates
-        if self.percentiles:
-            step = 0.0001 * (self.max - self.min)
-            for p in self.percentiles:
-                d = 1 if x - p[1] > 0 else -1
-                p[1] += step * (d + 2.0 * p[0] - 1.0)
-        ### 5- update exponential weighted averages
-        for alpha, val in self.expo_avgs.items():
-            self.expo_avgs[alpha] = x * alpha + val * (1 - alpha)
-
-
-class PyInterval(object):
-    pass  # TODO
-
-
-class PyDuration(object):
-    pass  # TODO:
-
-
 # keep buckets for intervals in size from 100ns to ~14 hours
 TIME_BUCKETS = sum( 
     [(1*10**x, 2*10**x, 5*10**x) for x in range(2, 13)], ())
@@ -157,147 +74,168 @@ ONE_HOUR_NS = 60 * ONE_MIN_NS
 
 WINDOW_COUNTS = [(64, ONE_MIN_NS), (32, ONE_HOUR_NS)]
 
-try:
-    import _faststat
+class _BaseStats(object):
+    'base class to avoid repeating code'
+    def __init__(self, buckets, lastN, percentiles, interval, expo_avgs,
+            window_counts, num_top):
+        buckets = buckets + (float("inf"),)
+        lastN = int(2**math.ceil(math.log(lastN)/math.log(2)))
+        num_top = int(2**math.ceil(math.log(lastN)/math.log(2)))
+        if interval:
+            self.interval = Interval(window_counts=())
+            interval = self.interval._stats
+        else:
+            interval = None
+        self._stats = _faststat.Stats(buckets, lastN, percentiles, interval,
+            expo_avgs, window_counts, num_top)
 
-    class _BaseStats(object):
-        'base class to avoid repeating code'
-        @property
-        def variance(self):
-            if self.n < 2:
-                return float("nan")
-            return self.m2 / (self.n - 1)
+    @property
+    def variance(self):
+        if self.n < 2:
+            return float("nan")
+        return self.m2 / (self.n - 1)
 
-        @property
-        def trimean(self):
-            p = self.get_percentiles()
-            return (p[0.25] + 2 * p[0.5] + p[0.75]) / 4
+    @property
+    def trimean(self):
+        p = self.get_percentiles()
+        return (p[0.25] + 2 * p[0.5] + p[0.75]) / 4
 
-        @property
-        def skewness(self):
-            if not self.m2:
-                return float("nan")
-            return self.n ** 0.5 * self.m3 / self.m2 ** 1.5
+    @property
+    def skewness(self):
+        if not self.m2:
+            return float("nan")
+        return self.n ** 0.5 * self.m3 / self.m2 ** 1.5
 
-        @property
-        def kurtosis(self):
-            if not self.m2:
-                return float("nan")
-            return self.n * self.m4 / self.m2 ** 2 - 3
+    @property
+    def kurtosis(self):
+        if not self.m2:
+            return float("nan")
+        return self.n * self.m4 / self.m2 ** 2 - 3
 
-        @property
-        def percentiles(self):
-            return self.get_percentiles()
+    @property
+    def percentiles(self):
+        return self.get_percentiles()
 
-        @property
-        def buckets(self):
-            return self.get_buckets()
+    @property
+    def buckets(self):
+        return self.get_buckets()
 
-        @property
-        def geometric_mean(self):
-            'nth root of product of data points'
-            return math.exp(self.sum_of_logs / self.n)
+    @property
+    def geometric_mean(self):
+        'nth root of product of data points'
+        return math.exp(self.sum_of_logs / self.n)
 
-        @property
-        def harmonic_mean(self):
-            'inverse of mean of inverses of data points'
-            return self.n / self.sum_of_inv
+    @property
+    def harmonic_mean(self):
+        'inverse of mean of inverses of data points'
+        return self.n / self.sum_of_inv
 
-        @property
-        def window_median(self):
-            if self.num_prev:
-                prev = sorted([self.get_prev(i)[1] for i in range(self.num_prev)])
-                return prev[len(prev)/2]
+    @property
+    def window_median(self):
+        if self.num_prev:
+            prev = sorted([self.get_prev(i)[1] for i in range(self.num_prev)])
+            return prev[len(prev)/2]
 
-        @property
-        def expo_avgs(self):
-            return self.get_expo_avgs()
+    @property
+    def expo_avgs(self):
+        return self.get_expo_avgs()
 
-        @property
-        def lag_avgs(self):
-            '''
-            same data as expo_avgs, but with keys as the average age
-            of the data -- assuming evenly spaced data points -- rather
-            than decay rates
-            '''
-            if not self.interval:
-                return
-            interval = self.interval.mean
-            return dict([(interval/alpha, val) 
-                for alpha, val in self.get_expo_avgs().items()])
-
-        def __repr__(self):
-            p = self.percentiles
-            if self.n < len(p):
-                quartiles = "(n too small)"
-            else:
-                quartiles = (_sigfigs(p.get(0.25, -1)), 
-                    _sigfigs(p.get(0.5, -1)), _sigfigs(p.get(0.75, -1)))
-            return '<faststat.{0} n={1} mean={2} quartiles={3}>'.format(
-                type(self).__name__, self.n, _sigfigs(self.mean), quartiles)
-
-
-    class CStats(_BaseStats):
+    @property
+    def lag_avgs(self):
         '''
-        Call add(value) to add a data point.
+        same data as expo_avgs, but with keys as the average age
+        of the data -- assuming evenly spaced data points -- rather
+        than decay rates
         '''
-        def __init__(self, buckets=DEFAULT_BUCKETS, lastN=64, percentiles=DEFAULT_PERCENTILES):
-            buckets = buckets + (float("inf"),)
-            lastN = int(2**math.ceil(math.log(lastN)/math.log(2)))
-            self.interval = CInterval(window_counts=())
-            self._stats = _faststat.Stats(buckets, lastN, percentiles, 
-                self.interval._stats, EXPO_AVGS, WINDOW_COUNTS, num_top=64)
-            self.add = self._stats.add
+        if not self.interval:
+            return
+        interval = self.interval.mean
+        return dict([(interval/alpha, val) 
+            for alpha, val in self.get_expo_avgs().items()])
 
-        def __getattr__(self, name):
-            if name not in ("tick", "end"):
-                return getattr(self._stats, name)
+    def __repr__(self):
+        p = self.percentiles
+        if self.n < len(p):
+            quartiles = "(n too small)"
+        else:
+            quartiles = (_sigfigs(p.get(0.25, -1)), 
+                _sigfigs(p.get(0.5, -1)), _sigfigs(p.get(0.75, -1)))
+        return '<faststat.{0} n={1} mean={2} quartiles={3}>'.format(
+            type(self).__name__, self.n, _sigfigs(self.mean), quartiles)
 
-    class CInterval(_BaseStats):
+
+class Stats(_BaseStats):
+    '''
+    Call add(value) to add a data point.
+    '''
+    def __init__(self, buckets=DEFAULT_BUCKETS, lastN=64, percentiles=DEFAULT_PERCENTILES,
+            interval=True):
+        super(Stats, self).__init__(buckets, lastN, percentiles, interval, EXPO_AVGS,
+            WINDOW_COUNTS, num_top=64)
+        self.add = self._stats.add
+
+    def __getattr__(self, name):
+        if name not in ("tick", "end"):
+            return getattr(self._stats, name)
+
+
+class Interval(_BaseStats):
+    '''
+    Call tick() to register occurrences.
+    Note that calling tick() N times results in N-1 data points.
+    '''
+    def __init__(self, buckets=TIME_BUCKETS, lastN=64, percentiles=DEFAULT_PERCENTILES,
+            window_counts=WINDOW_COUNTS):
+        super(Interval, self).__init__(buckets, lastN, percentiles, False, (), (), num_top=64)
+        self.tick = self._stats.tick
+
+    def __getattr__(self, name):
+        if name not in ("add", "end"):
+            return getattr(self._stats, name)
+
+
+class Duration(_BaseStats):
+    '''
+    Represents statistics for a duration.
+    Call end(start_time_nanos) to add a data point.
+    '''
+    def __init__(self, buckets=TIME_BUCKETS, lastN=64, percentiles=DEFAULT_PERCENTILES,
+            interval=True):
+        super(Duration, self).__init__(buckets, lastN, percentiles, interval, EXPO_AVGS,
+            WINDOW_COUNTS, num_top=64)
+        self.end = self._stats.end
+
+    def __getattr__(self, name):
+        if name not in ("add", "tick"):
+            return getattr(self._stats, name)
+
+
+class Markov(object):
+    '''
+    Represents the states of a Markov process.  The transition between states are
+    modeled as Intervals, and the time spent in a given state is modeled as
+    Durations.
+    '''
+    def __init__(self):
+        self.state_durations  = collections.defaultdict(faststat.Duration)
+        self.transition_intervals = collections.defaultdict(faststat.Interval)
+
+    def transition(self, nxt, cur=None, since=None):
         '''
-        Call tick() to register occurrences.
-        Note that calling tick() N times results in N-1 data points.
+        Register that a transition has taken place.
+        nxt is an identifier for the state being entered.
+        cur is an identifier for the state being left.
+        since is the time at which the previous state was entered.
         '''
-        def __init__(self, buckets=TIME_BUCKETS, lastN=64, percentiles=DEFAULT_PERCENTILES,
-                window_counts=WINDOW_COUNTS):
-            buckets = buckets + (float("inf"),)
-            lastN = int(2**math.ceil(math.log(lastN)/math.log(2)))
-            self._stats = _faststat.Stats(buckets, lastN, percentiles, 
-                None, (), window_counts, num_top=64)
-            self.tick = self._stats.tick
+        self.transition_intervals[(cur, nxt)].tick()
+        if since:
+            self.state_durations[cur].end(since)
 
-        def __getattr__(self, name):
-            if name not in ("add", "end"):
-                return getattr(self._stats, name)
 
-    class CDuration(_BaseStats):
-        '''
-        Call end(start_time_nanos) to add a data point.
-        '''
-        def __init__(self, buckets=TIME_BUCKETS, lastN=64, percentiles=DEFAULT_PERCENTILES):
-            buckets = buckets + (float("inf"),)
-            lastN = int(2**math.ceil(math.log(lastN)/math.log(2)))
-            self.interval = CInterval()
-            self._stats = _faststat.Stats(buckets, lastN, percentiles, 
-                self.interval._stats, EXPO_AVGS, WINDOW_COUNTS, num_top=64)
-            self.end = self._stats.end
+TimeSeries = functools.partial(Stats, interval=False)
 
-        def __getattr__(self, name):
-            if name not in ("add", "tick"):
-                return getattr(self._stats, name)
 
-    Stats = CStats
-    Interval = CInterval
-    Duration = CDuration
-    nanotime = _faststat.nanotime
-
-except ImportError:
-    CStats = None
-    CInterval = None
-    CDuration = None
-    Stats = PyStats
-    Interval = PyInterval
-    Duration = PyDuration
+nanotime = _faststat.nanotime
 
 
 def _sigfigs(n, sigfigs=3):
--- faststat/test.py
+++ faststat/test.py
@@ -4,7 +4,7 @@ http://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Higher-order_st
 '''
 import random
 import time
-from faststat import Stats, PyStats
+from faststat import Stats
 
 
 def p2_parabolic(l_v, l_n, c_v, c_n, r_v, r_n, d):
@@ -74,32 +74,5 @@ def test():
     return stats
 
 
-
-def compare():
-    random.seed(10)
-    if PyStats == Stats:
-        raise RuntimeError("C extension not installed -- nothing to compare!")
-    data = [random.normalvariate(1.0, 1.0) for i in range(int(1e6))]
-    cs = Stats()
-    py = PyStats()
-    #identical = 0
-    for i in range(len(data)):
-        d = data[i]
-        cs.add(d)
-        py.add(d)
-        if cs.m4 != py.m4:  # note: m4 is dependent on all earlier values
-            import pdb      # if m4 is the same, others are the same
-            pdb.set_trace()
-        '''
-        if c.m4 == py.m4:
-            identical += 1
-        if not c.m4 * 0.99 <= py.m4 <= c.m4 * 1.01:
-            raise ValueError("variation at point {0}: "
-                             "py={1}, c={2} (idential for {3} points)".format(
-                                    i, py.m4, c.m4, identical))
-        '''
-
-
 if __name__ == "__main__":
     test()
-    #test(py=True)
