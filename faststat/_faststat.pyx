2edf439de6829281ad1cf017e64a67c3f13bc2d8
Kurt Rose <=>
2016-06-09 01:03:09 -0700

    nailing down the interface between C and Cython; after filling in all of the algorithms, need to figure out the Cython / Python interface

--- faststat/_faststat.pyx
+++ faststat/_faststat.pyx
@@ -1,43 +1,164 @@
 from cpython.mem cimport PyMem_Malloc, PyMem_Free, PyMem_New
+from libc.math cimport log
 
 
 cdef extern from "faststat_core.c":
-    ctypedef faststat_Stats:
-        pass
-    int faststat_P2Percentile_size
+    ctypedef struct faststat_P2Percentile:
+        unsigned short percentile
+        double val
+        unsigned int n
+
+    ctypedef struct faststat_Bucket:
+        float max
+        unsigned int count
+
+    ctypedef struct faststat_DataPoint:
+        double value
+        unsigned long long nanotime
+
+    void faststat_P2Percentile_update(
+        faststat_P2Percentile *array, int num_percentiles, unsigned long long n,
+        double max, double min, double x)
+
+    void faststat_Bucket_update(faststat_Bucket *array, double x)
+
+    void faststat_DataPoint_update_topN(
+        faststat_DataPoint *topN, unsigned int num_top, double x, unsigned long long t)
+
+    void faststat_WindowCount_update(
+        faststat_WindowCount *window_counts, unsigned int num_window_counts, unsigned long long t)
+
+    float NAN
+
+
+ctypedef struct ExpoAvg:
+    double val
+    double alpha
 
 
 cdef class Stats:
     cdef:
-        faststat_Stats stats_struct
-        void *extra_heap = NULL
+        unsigned long long n
+        double mean, min, max, m2, m3, m4
+        double sum_of_logs, sum_of_inv
+        unsigned long long mintime, maxtime, lasttime
+        unsigned int num_percentiles
+        faststat_P2Percentile *percentiles
+        unsigned int num_buckets
+        faststat_Bucket *buckets
+        unsigned int num_expo_avgs
+        ExpoAvg *expo_avgs
+        double window_sum
+        unsigned int num_prev  # must be a power of 2
+        faststat_DataPoint *lastN
+        unsigned int num_top  # must be a power of 2
+        faststat_DataPoint *topN
+        unsigned int num_window_counts
+        faststat_WindowCount *window_counts
+        void *alloc = NULL
 
     def __cinit__(self, list buckets, list percentiles, Stats interval, 
             list expo_avgs, list window_counts, int num_top):
         cdef:
             size_t to_alloc = 0
             void *alloc = NULL
-            int percentile_offset
+            int percentile_offset, buckets_offset
 
+        # figure out size of memory block needed and allocate all at once
+        percentile_offset = to_alloc
         if percentiles:
-            percentile_offset = to_alloc
-            to_alloc += round_size(faststat_P2Percentile_size * len(percentiles))
+            to_alloc += round_size(sizeof(faststat_P2Percentile) * len(percentiles))
+        buckets_offset = to_alloc
+        if buckets:
+            if buckets[-1] != float('+inf'):
+                raise ValueError('last bucket must be positive infinity; found: {0}'.format(
+                    buckets[-1]))
+            to_alloc += round_size(sizeof(faststat_Bucket) * len(buckets))
+
+
 
 
         alloc = PyMem_Malloc(to_alloc * 4)
         if alloc == NULL:
             raise MemoryError  # could not allocate faststat memory
-        self.extra_heap = alloc
+        self.alloc = alloc
 
-        faststat_Stats_init(
-            &(self.stats_struct),
-            alloc + percentile_offset, len(percentiles),
-            alloc + )
+    def add(self, double x not None, unsigned long long t not None):
+        faststat_WindowCount_update(self.window_counts, self.num_window_counts, self.lasttime, t)
+        self.lasttime = t
+        self.n += 1
+        if self.n == 1:
+            self.min = self.max = x
+            self.mintime = self.maxtime = self.lasttime
+        if x <= self.min:
+            self.mintime = self.lasttime
+            self.min = x
+        if x >= self.max:
+            self.maxtime = self.lasttime
+            self.max = x
+        self.sum_of_logs += log(x) if x > 0 else NAN
+        self.sum_of_inv += 1 / x if x > 0 else NAN
+        self._update_moments(x)
+        faststat_P2Percentile_update(self.percentiles, self.num_percentiles, self.n, self.max, self.min, x)
+        faststat_Bucket_update(self.buckets)
+        self._update_expo_avgs(x)
+        faststat_DataPoint_update_topN(self.topN, self.num_top, x, t)
+        self._update_lastN(x)
 
+    cdef void _update_moments(self, double x):
+        '''
+        update mean, and 2nd, 3rd, and 4th moments
+        '''
+        cdef double n, delta, delta_n, delta_m2, delta_m3, delta_m4
+        n = self.n  # assign to double to ensure compiler keeps all intermediate results as floats
+        delta = x - self.mean
+        delta_n = delta / n
+        delta_m2 = delta * delta_n * (n - 1)
+        delta_m3 = delta_m2 * delta_n * (n - 2)
+        delta_m4 = delta_m2 * delta_n * delta_n * (n * (n - 3) + 3)
+        # compute updated values
+        self.mean = self.mean + delta_n
+        # note: order matters here
+        self.m4 += delta_m4 + delta_n * (6 * delta_n * self.m2 - 4 * self.m3)
+        self.m3 += delta_m3 + delta_n * 3 * self.m2
+        self.m2 += delta_m2
+
+    cdef void _update_expo_avgs(self, double x):
+        cdef:
+            unsigned int i
+            double val, alpha
+        for i in range(self.num_expo_avgs):
+            val = self.expo_avgs[i].val
+            alpha = self.expo_avgs[i].alpha
+            # this equation ensures no "gain"
+            self.expo_avgs[i].val = x * alpha + val * (1 - alpha)
+
+    cdef void _update_lastN(self, double x):
+        cdef unsigned int offset
+        if self.num_prev == 0:
+            return
+        # bit-wise or here is much faster than modulo, and correct
+        # as long as num_prev is a power of 2
+        offset = (self.n - 1) & (self.num_prev - 1)
+        self.window_sum -= self.lastN[offset].value
+        self.window_sum += x
+        self.lastN[offset].value = x
+        self.lastN[offset].nanotime = self.lasttime
+
+
+    property n:
+        'number of points seen'
+        def __get__(self): return self.n
+
+    property mean:
+        def __get__(self): return self.mean
+
+    property sum_of_logs:
+        def __get__(self): return self.sum_of_logs
 
     def __dealloc__(self):
-        if self.extra_heap:
-            PyMem_Free(self.extra_heap)
+        if self.alloc:
+            PyMem_Free(self.alloc)
 
 
 cdef size_t round_size(size_t size):
--- faststat/faststat_core.c
+++ faststat/faststat_core.c
@@ -51,7 +51,9 @@ static unsigned long long _nanotime(void) {
 
 #endif
 
+
 static unsigned long long nanotime_override = 0;
+
 
 static unsigned long long nanotime(void) {
     if(nanotime_override) {
@@ -59,6 +61,7 @@ static unsigned long long nanotime(void) {
     }
     return _nanotime();
 }
+
 
 //percentile point for usage in P2 algorithm
 typedef struct {
@@ -80,6 +83,7 @@ typedef struct {
     unsigned long long nanotime;
 } faststat_DataPoint;
 
+
 // represents an exponential moving average
 // aka a low pass filter, infinite impulse response filter
 typedef struct {
@@ -97,34 +101,7 @@ typedef struct {
 } faststat_WindowCount;
 
 
-// for representing a normally distributed variable
-typedef struct faststat_Stats_struct {
-    unsigned long long n;
-    double mean, min, max, m2, m3, m4;
-    double sum_of_logs, sum_of_inv;  // for geometric and harmonic mean
-    unsigned long long mintime, maxtime, lasttime;
-    unsigned int num_percentiles;
-    faststat_P2Percentile *percentiles;
-    unsigned int num_buckets;
-    faststat_Bucket *buckets; // last bucket MUST BE +inf
-    unsigned int num_expo_avgs;
-    faststat_ExpoAvg *expo_avgs;
-    double window_avg;
-    unsigned int num_prev; // MUST BE A POWER OF 2!
-    faststat_DataPoint *lastN;
-    unsigned int num_top; // MUST BE A POWER OF 2!
-    faststat_DataPoint *topN;
-    unsigned int num_window_counts;
-    //window counts must be sorted by window_size, to
-    //make handling code cleaner/smaller
-    faststat_WindowCount *window_counts;
-    struct faststat_Stats_struct *interval;
-} faststat_Stats;
 
-
-int faststat_Stats_init(faststat_Stats *self) {
-    *self = {0};
-}
 
 
 // round up to the nearest power of 2
@@ -144,4 +121,198 @@ unsigned int round_up_pwr_2(unsigned int v)
 }
 
 
-#define faststat_P2Percentile_size sizeof(faststat_P2Percentile)
+// be careful; if-condition must properly terminate when max == +inf, even for nan and +inf
+#define OFFSET(n)  if(!(x >= array[i+n].max)) { array[i+n].count++; break; } 
+
+void faststat_Bucket_update(faststat_Bucket *array, double x) {
+    unsigned int i;
+    for(i=0; ; i += 16) {
+        OFFSET( 0) OFFSET( 1) OFFSET( 2) OFFSET( 3) 
+        OFFSET( 4) OFFSET( 5) OFFSET( 6) OFFSET( 7) 
+        OFFSET( 8) OFFSET( 9) OFFSET(10) OFFSET(11) 
+        OFFSET(12) OFFSET(13) OFFSET(14) OFFSET(15)
+    }
+}
+
+
+//helper for _update_percentiles
+static void _p2_update_point(double l_v, double l_n, faststat_P2Percentile *cur,
+                            double r_v, double r_n, unsigned long long n) {
+    int d;
+    double percentile, new_val, c_v, c_n, diff;
+    percentile = ((double)cur->percentile) / 0x10000;
+    c_n = cur->n;
+    diff = (n - 1) * percentile + 1 - c_n;
+    // clamp d at +/- 1
+    if(diff >= 1) {
+        d = 1;
+    } else if(diff <= -1) {
+        d = -1;
+    } else {
+        return;
+    }
+    c_v = cur->val;
+    if(l_n < c_n + d && c_n + d < r_n) {  // try updating estimate with parabolic
+        new_val = c_v + (d / (r_n - l_n)) * ( 
+            (c_n - l_n + d) * (r_v - c_v) / (r_n - c_n) +
+            (r_n - c_n - d) * (c_v - l_v) / (c_n - l_n));
+        if(l_v >= new_val || r_v <= new_val) {  // fall back on linear
+            if(d == 1) {
+                new_val = c_v + (r_v - c_v) / (r_n - c_n);
+            } else {  // d == -1
+                new_val = c_v - (l_v - c_v) / (l_n - c_n);
+            }
+        }
+        cur->val = new_val;
+        cur->n += d;
+    }
+}
+
+
+static void _insert_percentile_sorted(
+        faststat_P2Percentile *array, int num_percentiles,
+        unsigned long long n, double x) {
+    unsigned long long num, i;  // prevent loss of precision compiler warning
+    double tmp;
+    num = n < num_percentiles ? n : num_percentiles;
+    for(i = 0; i < num-1; i++) { // insert in sorted order
+        if(x < percentiles[i].val) {
+            tmp = x;
+            x = percentiles[i].val;
+            percentiles[i].val = tmp;
+        }
+    }
+    percentiles[num-1].val = x;
+}
+
+
+//update percentiles using piece-wise parametric algorithm
+void faststat_P2Percentile_update(
+        faststat_P2Percentile *array, int num_percentiles, unsigned long long n,
+        double max, double min, double x) {
+    unsigned int i;
+    faststat_P2Percentile *right, *left, *cur, *prev, *nxt;
+    right = &(array[num_percentiles - 1]);
+    left = array;
+    if(!(right->n < n) ) { // just insert until n > num_percentiles
+        _insert_percentile_sorted(array, num_percentiles, n, x);
+        return;
+    }
+    //right-most is stopping case; handle first
+    if(x < right->val && right->n + 1 < n) {
+        right->n++;
+    }
+    //handle the rest of the points
+    prev = right;
+    for(i = num_percentiles-2; ; i--) {
+        cur = &(percentiles[i]);
+        if(x < cur->val && cur->n + 1 < prev->n) {
+            cur->n++;
+        }
+        prev = cur;
+        if(i == 0) { //making i unsigned fixes some warnings
+            break;
+        }
+    }
+    //left-most point is a special case
+    nxt = &(percentiles[1]);
+    _p2_update_point(min, 0, left, nxt->val, nxt->n, n);
+    cur = left;
+    for(i=1; i < num_percentiles - 1; i++) {
+        prev = cur;
+        cur = nxt;
+        nxt = &(percentiles[i+1]);
+        _p2_update_point(prev->val, prev->n, cur, nxt->val, nxt->n, n);
+    }
+    _p2_update_point(cur->val, cur->n, right, (double)max, (double)n, n);
+} 
+
+//this algorithm deterministically favors storing newer values over older values
+// expects topN to be a 1-based index to make 
+void faststat_DataPoint_update_topN(
+        faststat_DataPoint *topN, unsigned int num_top, double x, unsigned long long t) {
+    faststat_DataPoint *cur, *left, *right, *end, *min, *topN;
+    unsigned int cur_i, left_i, right_i;
+    if(x < topN->value) {
+        return;
+    }
+    topN -= 1; // switch to 1-based indexing to save additions navigating down heap
+    // (left child = 2 * index instead of 2 * index + 1; profiling found this to make a difference)
+    // replace the smallest element of the topN with the new point
+    topN[1].value = x;
+    topN[1].nanotime = t;
+    // restore the heap condition
+    cur = topN + 1;  // use pointers instead of array indices
+    cur_i = 1;
+    left = topN + 2;
+    left_i = 2;
+    right = topN + 3;
+    right_i = 3;
+    end = topN + 1 + self->num_top;
+    while(right < end) {
+        if(left->value == right->value) {
+            min = left->nanotime > right->nanotime ? left : right;
+        } else {
+            min = left->value < right->value ? left : right;
+        }
+        if(cur->value < min->value) {
+            break;
+        }
+        // swap cur with min of left, right
+        x = min->value;
+        t = min->nanotime;
+        min->value = cur->value;
+        min->nanotime = cur->nanotime;
+        cur->value = x;
+        cur->nanotime = t;
+        // set up for the next layer of the heap
+        cur = min;
+        cur_i = min == left ? left_i : right_i;
+        left_i = cur_i * 2;
+        right_i = left_i + 1;
+        left = topN + left_i;
+        right = topN + right_i;
+    }
+}
+
+
+// re-zero all of the windows which have been "missed" between self->lasttime and t
+static void _rezero_window_counts(
+        faststat_WindowCount *window_counts, unsigned int num_window_counts,
+        unsigned long long lasttime, unsigned long long t)) {
+    faststat_WindowCount *cur;
+    unsigned int i;
+    unsigned long long j, last_window, cur_window;
+    for(i = 0; i < num_window_counts; i++) {
+        cur = &(window_counts[i]);
+        last_window = lasttime / cur->window_size_nanosecs;
+        cur_window = t / cur->window_size_nanosecs;
+        if(last_window == cur_window) {
+            break;  // because window_counts are sorted by window_size_nanosecs,
+        }           // the next window cannot miss unless the current one does
+        if(cur_window - last_window >= cur->num_windows) {
+            memset(cur->counts, 0, sizeof(*cur->counts) * cur->num_windows);
+            continue;  // if the entire array is getting zero'd, just use memset
+        }
+        // TODO: convert this to a memset instead of a loop (perhaps)
+        for(j = last_window + 1; j <= cur_window; j++) {
+            cur->counts[j & (cur->num_windows - 1)] = 0;
+        }  // zero out all the "missed" windows
+    }
+}
+
+
+void faststat_WindowCount_update(
+        faststat_WindowCount *window_counts, unsigned int num_window_counts,
+        unsigned long long lasttime, unsigned long long t) {
+    faststat_WindowCount *cur;
+    unsigned int i, cur_count;
+    _rezero_window_counts(window_countx, num_window_counts, lasttime, t);
+    // step 2 -- increment current counts
+    for(i = 0; i < num_window_counts; i++) {
+        cur = &(window_counts[i]);
+        // use the current time as the index into the circular array to save some memory
+        cur_count = (t / cur->window_size_nanosecs) & (cur->num_windows - 1);
+        ++(cur->counts[cur_count]);
+    }
+}
