5767e769b9b2645e6f330972d2da557fc0dc0b80
Kurt Rose <kurt@kurtrose.com>
2025-07-22 22:03:21 +0200

    Expose package version

--- faststat/__init__.py
+++ faststat/__init__.py
@@ -2,3 +2,13 @@
 # using relative imports so ``import faststat`` works correctly.
 from .faststat import *
 from .format import *
+
+try:
+    from importlib.metadata import version as _version, PackageNotFoundError
+except ImportError:  # pragma: no cover - Python <3.8
+    from importlib_metadata import version as _version, PackageNotFoundError
+
+try:
+    __version__ = _version(__name__)
+except PackageNotFoundError:  # package is not installed
+    __version__ = "0.0.0"
